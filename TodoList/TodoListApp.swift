102660dd53954e6999a630d5cbcb19816aa7e7f6
Tyler Hu <tylerhu8@gmail.com>
2026-04-04 02:42:11 -0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Tyler Hu on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListTests/TodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListTests.swift
+//  TodoListTests
+//
+//  Created by Tyler Hu on 4/4/26.
+//
+
+import Testing
+@testable import TodoList
+
+struct TodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
